fdb9df3a372764f60cfc8e963bbcebd5c7afbbf0
IsratIJK <iptu.jahan.amin@gmail.com>
2021-02-07 21:35:46 +0600

    Find remainder solved

---
+++ FindRemainder.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int t,a,b,i,r;
+    scanf("%d",&t);
+    for(i=0;i<t;i++){
+        scanf("%d%d",&a,&b);
+        r=a%b;
+        printf("%d\n",r);
+    }
+    return 0;
+}
